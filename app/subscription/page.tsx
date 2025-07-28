c0d79fb7be28640cf196652e0284cdb7f032650a
Anas-7860 <anaskhan9501499079@gmail.com>
2025-07-28 21:58:53 +0530

    Implemented auth billing and create companinon

---
+++ app/subscription/page.tsx
@@ -0,0 +1,11 @@
+import { PricingTable } from "@clerk/nextjs"
+
+const page = () => {
+  return (
+    <div>
+      <PricingTable/>
+    </div>
+  )
+}
+
+export default page
---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import Navitems from './Navitems'
+import { SignInButton,SignedIn, SignedOut,UserButton } from '@clerk/nextjs'
+import { User } from 'lucide-react'
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+            <Image 
+            src="/images/logo.svg" 
+            alt="logo" 
+            width={46} 
+            height={44}
+            />
+        </div>
+        </Link>
+        <div className='flex items-center gap-8'>
+          <Navitems/>
+          <SignedOut>
+            <SignInButton>
+              <button className='btn-signin'>Sign in</button>
+              </SignInButton>
+
+          </SignedOut>
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
